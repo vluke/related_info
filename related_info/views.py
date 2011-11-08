ec012008f3145fb88181c9d657c97a25dd09603a
ryan.braganza@gmail.com <ryan.braganza@gmail.com@1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e>
2011-11-08 04:22:52 +0000

    add other info, table formatting (apps/related_info)
    git-svn-id: https://mytardis.googlecode.com/svn/apps/related_info/trunk@2188 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- related_info/views.py
+++ related_info/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.http import require_POST
 
 from tardis.tardis_portal.auth import decorators as authz
 from tardis.tardis_portal.creativecommonshandler import CreativeCommonsHandler
-from tardis.tardis_portal.models import Experiment
+from tardis.tardis_portal.models import Experiment, ExperimentParameter
 from tardis.tardis_portal.shortcuts import render_response_index
 
 
@@ -18,6 +18,8 @@ from .related_info import RelatedInfoHandler
 
 import logging
 logger = logging.getLogger(__name__)
+
+auxiliary_schema_namespace = 'http://www.tardis.edu.au/schemas/experiment/annotation/2011/07/07'
 
 def _redirect(experiment_id):
     return redirect(reverse('tardis.tardis_portal.views.view_experiment', args=[experiment_id]))
@@ -35,6 +37,8 @@ def index(request, experiment_id):
     c['related_uris'] = rih.uris()
     c['related_publications'] = rih.publications()
     c['experiment_id'] = int(experiment_id)
+
+    c['other_info'] = ExperimentParameter.objects.filter(name__schema__namespace=auxiliary_schema_namespace, parameterset__experiment=experiment_id)
 
     uris = RelatedInfoHandler(experiment_id)
 
